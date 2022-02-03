1b0a383171f36eabc481d03ae43192b198fa7337
sergei <gsvlobachev@gmail.com>
2022-02-03 05:37:28 +0300

    1

--- src/main/java/com/example/livebook/app/objects_of_world/personage/PersonageDefaultGenerator.java
+++ src/main/java/com/example/livebook/app/objects_of_world/personage/PersonageDefaultGenerator.java
@@ -4,26 +4,47 @@ import com.example.livebook.app.objects_of_world.personage.options.PersonageOpti
 import com.example.livebook.app.objects_of_world.personage.options.PersonageOptionsTemplate;
 import org.springframework.stereotype.Component;
 
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 @Component
 public class PersonageDefaultGenerator {
     private final PersonageOptionsTemplate personageOptionsTemplate;
     private final PersonageOptionsSurvival personageOptionsSurvival;
+    HashMap<String, Object> level1Map = new HashMap<>();
+    static int count = 0;
+    static int i = 0;
 
     public PersonageDefaultGenerator(PersonageOptionsTemplate personageOptionsTemplate, PersonageOptionsSurvival personageOptionsSurvival) {
         this.personageOptionsTemplate = personageOptionsTemplate;
         this.personageOptionsSurvival = personageOptionsSurvival;
     }
 
+    @SuppressWarnings("unchecked")
+    private static int findReplaceValueByKey(String[] directionOfMovementAlongTheTreePath, String key, int newValue, HashMap<String, Object> map) {//recursive HashMap traversal
+//        for (Object o : map.values()) {
+        map.forEach((k, v) -> {
+            if (v instanceof HashMap && directionOfMovementAlongTheTreePath[i].equals(k)) {
+                i++;
+                count = findReplaceValueByKey(directionOfMovementAlongTheTreePath, key, newValue, (HashMap<String, Object>) v);
+            }
+        });
+
+        if (map.replace(key, newValue) != null) {
+            count++;
+        }
+        return count;
+    }
+
     public void generatePersonageOptions(){
-        HashMap<String, Object> level1Map = new HashMap<>();
-       ;
+
         level1Map.put("bodyTemplate",  personageOptionsTemplate.getlevel1Map());
         level1Map.put("survival",  personageOptionsSurvival.getlevel1Map());
+        String[] directionOfMovementAlongTheTreePath = {"bodyTemplate", "organs", "density"};
 
+        System.out.println(findReplaceValueByKey(directionOfMovementAlongTheTreePath, "density", 1,level1Map));
         System.out.println(List.of(level1Map));
 
     }
+
+
 }
--- src/main/java/com/example/livebook/app/objects_of_world/personage/options/PersonageOptionsTemplate.java
+++ src/main/java/com/example/livebook/app/objects_of_world/personage/options/PersonageOptionsTemplate.java
@@ -11,7 +11,7 @@ public class PersonageOptionsTemplate {
     HashMap<String, Object> level2Map = new HashMap<>();
 
 
-    public HashMap<String, Object> level2Map() {
+    public HashMap<String, Object> getlevel1Map() {
         level2Map.put("density", 0);
         level2Map.put("block", 0);
         level2Map.put("resistance", 0);
@@ -20,17 +20,15 @@ public class PersonageOptionsTemplate {
         level2Map.put("smell", 0);
         level2Map.put("sound", 0);
         level2Map.put("color", "Color");
-        return level2Map;
-    }
 
-    public HashMap<String, Object> getlevel1Map() {
-        level2Map = level2Map();
+        level1Map.put("TEST", 0);
         level1Map.put("hair", level2Map);
         level1Map.put("skin", level2Map);
         level1Map.put("blood", level2Map);
         level1Map.put("bones", level2Map);
         level1Map.put("muscle", level2Map);
         level1Map.put("organs", level2Map);
+
         return level1Map;
     }
 
